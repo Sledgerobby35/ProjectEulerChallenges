89c60c8f5fb564b82699d3d8f46f00de48f7651d
Sledgerobby35 <sledge.robby35@gmail.com>
2023-06-11 21:32:23 -0500

    minor changes to code outline

---
+++ LargestPrimeFactors/javascript/index.js
@@ -0,0 +1,15 @@
+"use strict"
+
+$(window).on("load", function(){
+    document.getElementById("genLargest").addEventListener("click", function(){
+        generateLargest(600851475143);
+    })
+});
+
+function generateLargest(number){
+    var result = 0;
+    var i = 0;
+    for(result = 0; i < number; i++){
+        
+    }
+};
