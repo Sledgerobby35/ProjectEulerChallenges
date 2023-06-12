8815d93c882ee90fcecf70dd313df82668566570
Sledgerobby35 <sledge.robby35@gmail.com>
2023-06-11 22:10:55 -0500

    allow user to read console for result

--- LargestPrimeFactor/javascript/index.js
+++ LargestPrimeFactor/javascript/index.js
@@ -7,9 +7,19 @@ $(window).on("load", function(){
 });
 
 function generateLargest(number){
-    var result = 0;
-    var i = 0;
-    for(result = 0; i < number; i++){
-        
-    }
+    const factors = [];
+    var divisor = 2;
+
+    //do/while loop one lined
+    while (number >= 2) {
+        //if the remainder of number / divisor == 0. Add divisor to array number = number / divisor
+        if (number % divisor == 0) {
+          factors.push(divisor);
+          number = number / divisor;
+        } else {
+          divisor++;
+        }
+      }
+      console.log(factors);
+      return factors;
 };
