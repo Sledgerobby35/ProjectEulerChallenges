c717dab40b2cc9e86c806719b5a81961067e33dc
Sledgerobby35 <sledge.robby35@gmail.com>
2023-06-20 22:12:49 -0500

    build default html and js to solve challenge

---
+++ PowerDigitSum/javascript/index.js
@@ -0,0 +1,10 @@
+"use strict";
+$(window).on('load', function(){
+    document.getElementById('powerDigitSum').addEventListener('click', function(){
+        generatePowerDigitSum();
+    })
+})
+
+function generatePowerDigitSum(){
+    
+}
