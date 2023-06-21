65367b817922c070c710df6f96d7113acf94127e
Sledgerobby35 <sledge.robby35@gmail.com>
2023-06-20 22:52:48 -0500

    added functionality and achieved result

--- PowerDigitSum/javascript/index.js
+++ PowerDigitSum/javascript/index.js
@@ -6,5 +6,11 @@ $(window).on('load', function(){
 })
 
 function generatePowerDigitSum(){
-    
+    let result = 0;
+    let num = BigInt(2 ** 1000).toString().split('');
+    num.forEach(function(num){
+        let number = parseInt(num);
+        result += number;
+    })
+    console.log(result);
 }
